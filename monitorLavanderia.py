69183a69e666017c8d297d9ce9b0c426ecb6d40f
luiz <luizfelipeb487@gmail.com>
2026-02-24 17:25:26 -0300

    V2.0: Adicionada Lógica de memoria para avisar somente quando o equipamento volta a ficar online

--- monitorLavanderia.py
+++ monitorLavanderia.py
@@ -18,13 +18,14 @@ donoDaLavanderia = "Jason"
 
 # Ips definidos
 dispositivos = {
-    "192.168.1.20": "TOTEM DE PAGAMENTO",
-    "192.168.1.51": "MAQUINA 01",
+    #"192.168.1.20": "TOTEM DE PAGAMENTO",
+    #"192.168.1.51": "MAQUINA 01",
     "192.168.18.93": "MAQUINA 02", # teste com o ip do meu celular
     "192.168.18.1": "MAQUINA 03", # gateway do meu modem pra teste
-    "192.168.1.54": "MAQUINA 04"
+    #"192.168.1.54": "MAQUINA 04"
 }
 
+estadosAnteriores = {ip: True for ip in dispositivos}
 
 def enviarTelegram(mensagem):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
@@ -46,31 +47,41 @@ def verificar_maquinas():
 
     
 
-    print(f"\n------Entendido - Iniciando Checagem de {minutos} em {minutos} minuto(s): ------{time.strftime('%H:%M:%S')} ---")
+    print(f"\nIniciando Checagem de {minutos} em {minutos} minuto(s):----{time.strftime('%H:%M:%S')} ---")
     print("Pressione Ctrl + c para parar o programa.")
     
     for ip, nome in dispositivos.items():
-        # O comando 'ping' envia um sinal e espera resposta
-        # '-n 1' envia apenas 1 pacote para ser rápido
-        # '-w 500' espera meio segundo pela resposta
         comando = subprocess.run(['ping', '-n', '1', '-w', '500', ip], capture_output=True)
-        
-        if comando.returncode == 0:
-            print(f"✅ {nome} ({ip}) está Online.")
-        else:
-            print(f"❌ ATENÇÃO: {nome} ({ip}) ESTÁ OFFLINE!")
-            # da pra colocar o código para enviar um WhatsApp
+        onlineAgora = (comando.returncode == 0)
+
+        # Ficou Offline
+        if not onlineAgora and estadosAnteriores[ip]:
+            print(f"❌ {nome} ({ip}) Está OFFLINE!")
 
             with open("logLavanderia.txt", "a") as arquivo:
-                    arquivo.write(f"{time.strftime('%d/%m/%Y %H:%M:%S')} - ERRO: {nome} ({ip}) fora do ar\n")
+                arquivo.write(f"{time.strftime('%d/%m/%Y %H:%M:%S')} - ERRO: {nome} fora do ar\n")
 
-            mensagem = f"⚠️*ALERTA DO SISTEMA:* {donoDaLavanderia}, {nome} ip ({ip}) parou de responder! ⚠️"
+                mensagem = f"*ALERTA:* {donoDaLavanderia}, a *{nome}* parou de responder! ⚠️"
+                enviarTelegram(mensagem)
+                estadosAnteriores[ip] = False #atualiza a memoria para off
+
+        # Ficou online
+        elif onlineAgora and not estadosAnteriores[ip]:
+            print(f"✅ {nome} ({ip}) VOLTOU A FICAR ONLINE!")
+
+            mensagem = f"🎉 *BOAS NOTÍCIAS:* {donoDaLavanderia}, a *{nome}* está online novamente!"
             enviarTelegram(mensagem)
+            estadosAnteriores[ip] = True # Atualiza a memoria para on
+
+        else:
+
+            status = "Online" if onlineAgora else "Offline"
+            print(f"ℹ️ {nome}: Mantém-se {status}")    
 
             # kit.sendwhatmsg_instantly("+5531995551028", mensagem, wait_time=15, tab_close=True)
 
+print(f"Olá! O sistema de monitoramento da lavanderia do {donoDaLavanderia} está pronto.")
 
-#print(f"Olá! O sistema de monitoramento da lavanderia do {donoDaLavanderia} está pronto.")
 #minutos = input("De quantos em quantos minutos devo checar as máquinas?  ")
 #intervaloSegundos = int(minutos) * 60
 
@@ -95,11 +106,13 @@ def configTempo():
 
 #print(f" Configuração carregada: Checagem a cada {minutos} minuto(s).")
 
+estavaOnline = True
+
 while True:
     
     minutos = configTempo()
-    intervaloSegundos = int(minutos) * 60
-
     verificar_maquinas()
+
     print(f"\n Aguardando {minutos} minuto(s) para a próxima checagem...")
-    time.sleep(intervaloSegundos)
+    time.sleep(minutos * 60)
+             
